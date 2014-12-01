c244bf67923ab027f94d52c2b4a10949765cce7f
Adrian Nackov <anackov@gmail.com>
2014-12-01 19:15:05 +0200

    before the main algorithm

--- sud.js
+++ sud.js
@@ -1,11 +1,6 @@
-/* Copyright 2014 Adrian Nackov
- * Released under BSD Licence (2 clause):
- * http://www.opensource.org/licenses/bsd-license.php
- */
-
 var SUDOKU = SUDOKU || {};
         
-SUDOKU.board = (function() {
+SUDOKU.board_math = (function() {
     // a board is represented as a string of nums and dots,
     // board = "1.2...3452..451..6" <- first and second row
     // a board has 9 rows, 9 columns (and thus 9 squares of 9 digits each)
@@ -14,6 +9,13 @@ SUDOKU.board = (function() {
     function _get_row(i) { return parseInt(i / 9); }
     function _get_square_row(i) { return parseInt(_get_row(i) / 3); }
     function _get_square_column(i) { return parseInt(_get_column(i) / 3); }
+
+    function _unique(nums) {
+        var unique = {};
+        return nums.filter(function(num) {
+            return (num in unique) ? false : (unique[num] = true);
+        });
+    }
 
     function get_row_indexes(index) {
         var same_row_indexes = [];
@@ -36,48 +38,83 @@ SUDOKU.board = (function() {
         var row = _get_square_row(index);
         var col = _get_square_column(index);
         for (var i = 0; i < 9 * 9; i++) {
-            if (_get_square_row(i) == row && _get_square_column(i) == col) {
+            if (_get_square_row(i) === row && _get_square_column(i) === col) {
                 same_square_indexes.push(i);
             }
         }
         return same_square_indexes;
     }
 
+    function get_related_indexes(index) {
+        var indexes = get_row_indexes(index);
+        indexes = indexes.concat(get_column_indexes(index));
+        indexes = indexes.concat(get_square_indexes(index));
+        indexes = _unique(indexes).sort();
+        return indexes;
+    }
+
     return {
         get_row_indexes: get_row_indexes,
         get_column_indexes: get_column_indexes,
-        get_square_indexes: get_square_indexes
+        get_square_indexes: get_square_indexes,
+        get_related_indexes: get_related_indexes
     };
 })();
 
 
 SUDOKU.solver = (function() {
-    var _nums = "";
+    // javascript strings are immutable, this fn does "str[i] = new_val"
+    function _str_replace_at(i, new_val, str) {
+        return str.substr(0, i) + new_val + str.substr(i + 1);
+    }
 
-    function _get_possible_nums(index) {
-        var possible_nums = [];
-        // TODO
+    function _get_all_possible_nums(board) {
+        var possible_nums = {};
+        for (var index = 0; index < 80; index++)
+            if (board[index] === ".")
+                possible_nums[index] = get_possible_nums(index, board);
         return possible_nums;
     }
 
-    function init(nums) { _nums = nums; }
-
-    function solve() {
-        // board_with_nums = "14.23.6.." etc.
-        for (var i = 0; i < 80; i++) {
-            if (_nums[i] == ".") {
-                var solutions = _get_possible_nums(i);
-                if (solutions.length == 1) {
-                    board_with_nums[i] = solutions[0];
-                    i = 0; // restart the loop
-                }
+    function _update_solutions(possible_nums, board) {
+        for (var index in possible_nums) {
+            var nums = possible_nums[index];
+            if (nums.length === 1) {
+                index = parseInt(index);
+                board = _str_replace_at(index, nums[0], board);
             }
         }
-        // TODO
+        return board;
+    }
+
+    function get_possible_nums(index, board) {
+        var indexes = SUDOKU.board_math.get_related_indexes(index);
+        var indexes_with_nums = indexes.filter(
+            function(i) { return board[i] !== "."; }
+        );
+        var nums = indexes_with_nums.map(
+            function(i) { return parseInt(board[i]); }
+        );
+        return [1,2,3,4,5,6,7,8,9].filter(
+            function(num) { return nums.indexOf(num) === -1; }
+        );
+    }
+
+    function solve(board) {
+        while (board.indexOf(".") > -1) {
+            var possible_nums = _get_all_possible_nums(board);
+            board = _update_solutions(possible_nums, board);
+            console.log(board);
+            break;
+            if (board.indexOf(".") > -1) {
+                // TODO backtracking
+            }
+        }
+        return board;
     }
 
     return {
-        init: init,
+        get_possible_nums: get_possible_nums,
         solve: solve
     };
 })();
--- sud_tests.js
+++ sud_tests.js
@@ -1,71 +1,81 @@
-/* Copyright 2014 Adrian Nackov
- * Released under BSD Licence (2 clause):
- * http://www.opensource.org/licenses/bsd-license.php
- */
-
-QUnit.test("SUDOKU.board.get_row_indexes", function(assert) {
+QUnit.test("SUDOKU.board_math.get_row_indexes", function(assert) {
     // test first row
-    assert.deepEqual(SUDOKU.board.get_row_indexes(0),
+    assert.deepEqual(SUDOKU.board_math.get_row_indexes(0),
                     [0, 1, 2, 3, 4, 5, 6, 7, 8],
                     "First row indexes, begining(0)");
-    assert.deepEqual(SUDOKU.board.get_row_indexes(5),
+    assert.deepEqual(SUDOKU.board_math.get_row_indexes(5),
                     [0, 1, 2, 3, 4, 5, 6, 7, 8],
                     "First row indexes, middle(5)");
-    assert.deepEqual(SUDOKU.board.get_row_indexes(8),
+    assert.deepEqual(SUDOKU.board_math.get_row_indexes(8),
                     [0, 1, 2, 3, 4, 5, 6, 7, 8],
                     "First row indexes, end(8)");
     // test intermediate (second) row
-    assert.deepEqual(SUDOKU.board.get_row_indexes(9),
+    assert.deepEqual(SUDOKU.board_math.get_row_indexes(9),
                     [9, 10, 11, 12, 13, 14, 15, 16, 17],
                     "Second row indexes, beginning(9)");
-    assert.deepEqual(SUDOKU.board.get_row_indexes(17),
+    assert.deepEqual(SUDOKU.board_math.get_row_indexes(17),
                     [9, 10, 11, 12, 13, 14, 15, 16, 17],
                     "Second row indexes, end(17)");
     // test last row
-    assert.deepEqual(SUDOKU.board.get_row_indexes(80),
+    assert.deepEqual(SUDOKU.board_math.get_row_indexes(80),
                     [72, 73, 74, 75, 76, 77, 78, 79, 80],
                     "Last row indexes, end(80)");
-    assert.deepEqual(SUDOKU.board.get_row_indexes(72),
+    assert.deepEqual(SUDOKU.board_math.get_row_indexes(72),
                     [72, 73, 74, 75, 76, 77, 78, 79, 80],
                     "Last row indexes, begining(72)");
 });
 
-QUnit.test("SUDOKU.board.get_column_indexes", function(assert) {
+QUnit.test("SUDOKU.board_math.get_column_indexes", function(assert) {
     // test first column
-    assert.deepEqual(SUDOKU.board.get_column_indexes(0),
+    assert.deepEqual(SUDOKU.board_math.get_column_indexes(0),
                     [0, 9, 18, 27, 36, 45, 54, 63, 72],
                     "First column indexes, beginning(0)");
-    assert.deepEqual(SUDOKU.board.get_column_indexes(72),
+    assert.deepEqual(SUDOKU.board_math.get_column_indexes(72),
                     [0, 9, 18, 27, 36, 45, 54, 63, 72],
                     "First column indexes, end(72)");
     // test intermediate (third) column
-    assert.deepEqual(SUDOKU.board.get_column_indexes(2),
+    assert.deepEqual(SUDOKU.board_math.get_column_indexes(2),
                     [2, 11, 20, 29, 38, 47, 56, 65, 74],
                     "Third column indexes, beginning(2)");
-    assert.deepEqual(SUDOKU.board.get_column_indexes(74),
+    assert.deepEqual(SUDOKU.board_math.get_column_indexes(74),
                     [2, 11, 20, 29, 38, 47, 56, 65, 74],
                     "Third column indexes, end(74)");
     // test last column
-    assert.deepEqual(SUDOKU.board.get_column_indexes(80),
+    assert.deepEqual(SUDOKU.board_math.get_column_indexes(80),
                     [8, 17,26, 35, 44, 53, 62, 71, 80],
                     "Last column indexes, end(80)");
 
 });
 
-QUnit.test("SUDOKU.board.get_same_square_indexes", function(assert) {
+QUnit.test("SUDOKU.board_math.get_same_square_indexes", function(assert) {
     // test left-top square (first square of 9)
-    assert.deepEqual(SUDOKU.board.get_square_indexes(9),
+    assert.deepEqual(SUDOKU.board_math.get_square_indexes(9),
                     [0, 1, 2, 9, 10, 11, 18, 19, 20],
                     "Left-top square indexes, middle(9)");
-    assert.deepEqual(SUDOKU.board.get_square_indexes(20),
+    assert.deepEqual(SUDOKU.board_math.get_square_indexes(20),
                     [0, 1, 2, 9, 10, 11, 18, 19, 20],
                     "Left-top square indexes, end(20)");
-    assert.deepEqual(SUDOKU.board.get_square_indexes(80),
+    assert.deepEqual(SUDOKU.board_math.get_square_indexes(80),
                     [60, 61, 62, 69, 70, 71, 78, 79, 80],
                     "Bottom-right square indexes, end(80)");
 });
 
+// TODO - test get_related_indexes();
+
+// -------------------------------------------------------------
+// one public domain sudoku, from sourceforge.net/projects/winsudoku/
+var TEST_BOARD = "....2.6.19..8..5....6..3..26184....5.........7.5...9.4.....8.1.....14..9.3.9.....";
+
+QUnit.test("SUDOKU.solver.get_possible_nums", function(assert) {
+    assert.deepEqual(SUDOKU.solver.get_possible_nums(0, TEST_BOARD),
+                    [3, 4, 5, 8],
+                    "possible nums, top-left corner");
+    assert.deepEqual(SUDOKU.solver.get_possible_nums(80, TEST_BOARD),
+                    [6, 7, 8],
+                    "possible nums, bottom-right corner");
+});
+
+// TODO
 QUnit.test("SUDOKU.solver.solve", function(assert) {
-    SUDOKU.solver.init("..."); // TODO
-    assert.deepEqual(SUDOKU.solver.solve(), "", "ERROR");
+    assert.deepEqual(SUDOKU.solver.solve(TEST_BOARD), "", "ERROR");
 });
